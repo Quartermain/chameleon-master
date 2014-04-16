aca87b8dbddcd82e82395cccf2ebccd12af380ce
trungjc <trungnguyenthanh70@gmail.com>
2014-04-16 10:53:34 +0700

    update form search

--- wp-content/themes/gallery/searchform.php
+++ wp-content/themes/gallery/searchform.php
@@ -7,7 +7,7 @@
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
    
-	<label class="screen-reader-text"><?php _x( 'Search for:', 'label', 'gallery' ); ?></label>
+	<label class="screen-reader-text"><span style="">KEEP UPDTED WITH US</span></label>
 
 	<input type="text" class="search-field" placeholder="<?php echo esc_attr_x( 'Search', 'placeholder', 'gallery' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
 
